fe31be4b829212bc4c80fa74ba600c09e39ae1cc
kkaylee <kaylee.kappes@patelhs.org>
2020-11-09 17:38:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    int[] students = new int[15];
+
+    for (int i = 0; i < students.length; i++) {
+      System.out.println("What is your name?");
+
+      students[i] = .nextLine();
+    }
+
+  }
+}
